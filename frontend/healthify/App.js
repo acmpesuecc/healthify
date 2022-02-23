6665df280f31b090450f274db3ca148b9d78cc4d
anirudhlakhotia <sanjeev196945@gmail.com>
2022-02-23 14:55:46 +0530

    :tada: Frontend init
    Blank React Native app

---
+++ frontend/healthify/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is Healthify!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize: 30,
+    color: '#99dfb2',
+  }
+});
