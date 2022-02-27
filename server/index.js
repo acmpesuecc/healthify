9d67f4ba4d6224a732b7847216bd54c24723581c
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-02-28 00:02:13 +0530

    Login

--- server/index.js
+++ server/index.js
@@ -62,9 +62,39 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   try {
-    console.log("Login route called!");
+    // Get user input
+    const { email, password } = req.body;
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("An email and password is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    } else {
+      res
+        .status(400)
+        .send(
+          "Invalid Credentials: Either user does not exist or password is incorrect"
+        );
+    }
   } catch (err) {
     console.log(err);
   }
