9a814dc111ae8856150a935fec387a42372a017e
anirudhlakhotia <sanjeev196945@gmail.com>
2022-03-04 19:23:58 +0530

    :sparkles: Added No History Card to Dashboard

--- frontend/healthify/containers/Dashboard.js
+++ frontend/healthify/containers/Dashboard.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Dimensions, LogBox } from "react-native";
+import { StyleSheet, View, Dimensions, LogBox, Image, Text, TouchableOpacity } from "react-native";
 import ActionButton from "react-native-action-button";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import React, { useEffect } from "react";
@@ -10,8 +10,15 @@ export default function Dashboard() {
   }, []);
   return (
     <View style={styles.container}>
+      <TouchableOpacity disabled style={styles.recentCard}>
+        <Text style={styles.heading}>You have no search history</Text>
+        <Text style={styles.subHeading}>
+          You can search for medicine prices by clicking on the search button on the bottom right corner
+          </Text>
+        <Image source={require("./assets/wait.png")} style={styles.image} />
+      </TouchableOpacity>
       <ActionButton
-        buttonColor={(active) => (active ? "#99dfb2" : "#99dfb2")}
+        buttonColor="#2d3b6c"
         size={0.15 * width}
         renderIcon={(active) =>
           active ? (
@@ -20,16 +27,17 @@ export default function Dashboard() {
             <Icon name="search" style={styles.actionButtonIcon} />
           )
         }
+        style={styles.actionButton}
       >
         <ActionButton.Item
-          buttonColor="#99dfb2"
+          buttonColor="#2d3b6c"
           title="Scan the name of the medicine"
           onPress={() => console.log("Scan")}
         >
           <Icon name="camera-alt" style={styles.actionButtonIcon} />
         </ActionButton.Item>
         <ActionButton.Item
-          buttonColor="#99dfb2"
+          buttonColor="#2d3b6c"
           title="Type in the name of the medicine"
           onPress={() => console.log("Type")}
         >
@@ -43,7 +51,7 @@ export default function Dashboard() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#85b8cb",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -51,9 +59,39 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: "#99dfb2",
   },
+  heading: {
+    fontSize: 0.03 * height,
+    fontWeight: "bold",
+    marginTop: 0.05 * height,
+    color: "#2d3c6c",
+    
+  },
+  subHeading: {
+    fontSize: 0.015 * height,
+    color: "#5b7792",
+    justifyContent: "center",
+    textAlign: "center",
+    padding: 0.05 * height,
+
+  },
   actionButtonIcon: {
     fontSize: 0.03 * height,
     height: 22,
     color: "white",
   },
+  recentCard: {
+    flex: 0.6,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    width: width * 0.9,
+    borderRadius: 0.05 * width,
+
+  },
+  image: {
+    flex: 0.8,
+    resizeMode: "contain",
+    width: width * 0.9,
+    height: height,
+  }
 });
