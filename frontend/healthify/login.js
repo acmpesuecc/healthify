4d63895a4bfa6a5e24600d04841859f3f913a938
anirudhlakhotia <sanjeev196945@gmail.com>
2022-03-09 21:19:25 +0530

    :sparkles: Added Login from frontend (without token setting)

--- frontend/healthify/login.js
+++ frontend/healthify/login.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -9,10 +9,15 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
+import axios from "axios";
 
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    setErrorMessage("");
+  }, [email, password]);
 
   return (
     <View style={styles.container}>
@@ -41,8 +46,36 @@ export default function App() {
       <TouchableOpacity>
         <Text style={styles.forgot_button}>Forgot Password?</Text>
       </TouchableOpacity>
+      <View style={{ marginBottom: 20 }}>
+        <Text>
+          {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
+        </Text>
+      </View>
 
-      <TouchableOpacity style={styles.loginBtn}>
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={async () => {
+          await axios
+            .post(
+              "http://192.168.1.18:3001/login",
+              { email: email, password: password },
+              {
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json;charset=UTF-8",
+                },
+              }
+            )
+            .then((response) => {
+              console.log(response.status);
+              console.log(response.data.token);
+            })
+            .catch((error) => {
+              setErrorMessage(error.response.data);
+              console.log(errorMessage);
+            });
+        }}
+      >
         <Text style={styles.loginText}>LOGIN</Text>
       </TouchableOpacity>
     </View>
@@ -65,7 +98,7 @@ const styles = StyleSheet.create({
   },
 
   inputView: {
-    backgroundColor: "#FFC0CB",
+    backgroundColor: "#f2f2f2",
     borderRadius: 30,
     width: "70%",
     height: 45,
@@ -75,7 +108,7 @@ const styles = StyleSheet.create({
 
   TextInput: {
     flex: 1,
-    textAlign: "center",
+    textAlign: "left",
   },
 
   forgot_button: {
@@ -89,6 +122,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 10,
-    backgroundColor: "#FF1493",
+    backgroundColor: "#00B0FF",
+  },
+  error: {
+    color: "red",
+    fontSize: 16,
   },
 });
--- server/index.js
+++ server/index.js
@@ -71,30 +71,33 @@ app.post("/login", async (req, res) => {
       res.status(400).send("An email and password is required");
     }
     // Validate if user exist in our database
-    const user = await User.findOne({ email });
+    else {
+      const user = await User.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-
-      // save user token
-      user.token = token;
-
-      // user
-      res.status(200).json(user);
-    } else {
-      res
-        .status(400)
-        .send(
-          "Invalid Credentials: Either user does not exist or password is incorrect"
+      if (user && (await bcrypt.compare(password, user.password))) {
+        // Create token
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
         );
+  
+        // save user token in database
+        user.token = token;
+  
+        // user
+        res.status(200).json({user:user,token:token});
+      } else {
+        console.log("User not found");
+        res.status(400)
+          .send(
+            "Invalid Credentials: Either user does not exist or password is incorrect"
+          );
+      }
     }
+  
   } catch (err) {
     console.log(err);
   }
