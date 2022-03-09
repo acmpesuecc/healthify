27de81497fdd03d622f60e8c75ed7db936aac97f
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-03-09 18:38:48 +0530

    Login design

--- frontend/healthify/containers/WelcomeScreen.js
+++ frontend/healthify/containers/WelcomeScreen.js
@@ -1,36 +1,37 @@
-
-import { StyleSheet, Image, View, Dimensions, TouchableOpacity , Text} from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import { Button, Card } from "react-native-elements";
 var { height, width } = Dimensions.get("window");
 
 export default function WelcomeScreen() {
   return (
     <View style={styles.container}>
-
-      <Text style={styles.text}>
-        Welcome to Healthify
-      </Text>
+      <Text style={styles.text}>Welcome to Healthify</Text>
       <Image
         source={require("./assets/hi.gif")}
         style={{
           resizeMode: "contain",
-          width: 1.2*width,
-          height: 0.8*height,
-          marginTop:0.3*height,
+          width: 1.2 * width,
+          height: 0.8 * height,
+          marginTop: 0.3 * height,
         }}
       />
-          <Button
-          title="Get Started"
-          buttonStyle={styles.button}
-          onPress={() => {
-            console.log("Pressed");
-          }}
-        />
+      <Button
+        title="Get Started"
+        buttonStyle={styles.button}
+        onPress={() => {
+          console.log("Pressed");
+        }}
+      />
       {/* <Text style={{fontSize: 0.03*height,fontWeight:"bold",color:"#1d2366",}}>
         Compare Medicine Prices 
       </Text> */}
-
-
     </View>
   );
 }
@@ -43,8 +44,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
-    position:"absolute", 
-    top:0.1*height,
+    position: "absolute",
+    top: 0.1 * height,
     fontSize: 30,
     color: "#2d3b6c",
     fontFamily: "sans-serif",
@@ -54,19 +55,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#2d3b6c",
     borderRadius: 0.2 * width,
     width: 0.7 * width,
-    marginBottom: 0.3*height,
+    marginBottom: 0.3 * height,
     // position:"absolute",
     // alignItems: "center",
     // justifyContent: "center",
   },
-  bottomCard:{
+  bottomCard: {
     backgroundColor: "white",
-    width : width,
-    height: 0.4*height,
+    width: width,
+    height: 0.4 * height,
     borderRadius: 0.1 * width,
     alignItems: "center",
     position: "absolute",
     bottom: -0.5 * height,
-
-  }
+  },
 });
--- frontend/healthify/login.js
+++ frontend/healthify/login.js
@@ -1,4 +1,3 @@
-
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import {
@@ -10,46 +9,46 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
- 
+
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- 
+
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("./assets/owl.png")} />
- 
+      <Image style={styles.image} source={require("./assets/login.png")} />
+
       <StatusBar style="auto" />
       <View style={styles.inputView}>
         <TextInput
           style={styles.TextInput}
-          placeholder="Email."
+          placeholder="Email"
           placeholderTextColor="#003f5c"
           onChangeText={(email) => setEmail(email)}
         />
       </View>
- 
+
       <View style={styles.inputView}>
         <TextInput
           style={styles.TextInput}
-          placeholder="Password."
+          placeholder="Password"
           placeholderTextColor="#003f5c"
           secureTextEntry={true}
           onChangeText={(password) => setPassword(password)}
         />
       </View>
- 
+
       <TouchableOpacity>
         <Text style={styles.forgot_button}>Forgot Password?</Text>
       </TouchableOpacity>
- 
+
       <TouchableOpacity style={styles.loginBtn}>
         <Text style={styles.loginText}>LOGIN</Text>
       </TouchableOpacity>
     </View>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -57,40 +56,39 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
- 
+
   image: {
     marginBottom: 40,
+    width: "100%",
+    height: undefined,
+    aspectRatio: 1,
   },
- 
+
   inputView: {
     backgroundColor: "#FFC0CB",
     borderRadius: 30,
     width: "70%",
     height: 45,
     marginBottom: 20,
- 
     alignItems: "center",
   },
- 
+
   TextInput: {
-    height: 50,
     flex: 1,
-    padding: 10,
-    marginLeft: 20,
+    textAlign: "center",
   },
- 
+
   forgot_button: {
-    height: 30,
-    marginBottom: 30,
+    marginBottom: 70,
   },
- 
+
   loginBtn: {
     width: "80%",
     borderRadius: 25,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 40,
+    marginBottom: 10,
     backgroundColor: "#FF1493",
   },
 });
