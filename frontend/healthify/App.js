64ec76706cd1c7e3f5abc9947567e358027132c3
anirudhlakhotia <sanjeev196945@gmail.com>
2022-03-10 11:56:51 +0530

    :sparkles: Added Verify email page

--- frontend/healthify/App.js
+++ frontend/healthify/App.js
@@ -1,24 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Image, View, Dimensions, Text } from "react-native";
+const { width, height } = Dimensions.get("window");
+import Verify from "./Verify";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is Healthify!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Verify email = "examplemail@gmail.com"/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text:{
-    fontSize: 30,
-    color: '#99dfb2',
-  }
+  text: {
+    fontSize: 20,
+    color: "#99dfb2",
+  },
+  logo: {
+    width: 0.8 * width,
+    height: 0.8 * width,
+  },
 });
+export default App;
