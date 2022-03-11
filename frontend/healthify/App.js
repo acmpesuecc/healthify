266db4a2e66da453b76049fbeb765f67be6c4f59
Skill Kumar <skillkumaroriginal@gmail.com>
2022-03-11 23:34:24 +0530

    sign-up page with validation (using class components)

--- frontend/healthify/App.js
+++ frontend/healthify/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUp from './containers/SignUp';
+// import ValidationComponent from 'react-native-form-validator'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is Healthify!</Text>
+    <View style={styles.SignUp}>
+      {/* <Text style={styles.text}>This is Healthify!</Text> */}
+    <SignUp/>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -20,5 +24,14 @@ const styles = StyleSheet.create({
   text:{
     fontSize: 30,
     color: '#99dfb2',
+  },
+  SignUp: {
+    flex: 1,
+    backgroundColor: '#2dcca4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 60,
+    paddingRight: 60 
   }
 });
+
