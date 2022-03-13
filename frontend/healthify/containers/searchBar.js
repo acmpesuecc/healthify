9cbfa3798290b92293612399560736a53a8a8fc7
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-03-13 23:28:29 +0530

    Added suggestions

--- frontend/healthify/containers/searchBar.js
+++ frontend/healthify/containers/searchBar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
+  Text,
   TextInput,
   View,
   Keyboard,
@@ -8,6 +9,7 @@ import {
   Dimensions,
 } from "react-native";
 import { Feather, Entypo } from "@expo/vector-icons";
+import axios from "axios";
 var { height, width } = Dimensions.get("window");
 
 const SearchBar = () => {
@@ -15,28 +17,49 @@ const SearchBar = () => {
   const [meds, setMeds] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
 
+  useEffect(() => {
+    const loadMeds = async () => {
+      const response = await axios.get(
+        `https://www.mims.com/autocomplete?countryCode=IN&query=${text}`
+      );
+      setMeds(response.data.suggestions.slice(0, 5));
+      // console.log(meds[0].value);
+    };
+    loadMeds();
+  });
+
   const onChangeHandler = (text) => {
     setText(text);
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.searchBar}>
-        {/* search Icon */}
-        <Feather
-          name="search"
-          size={20}
-          color="black"
-          style={{ marginLeft: 1 }}
-        />
-        {/* Input field */}
-        <TextInput
-          style={styles.input}
-          placeholder="Search"
-          value={text}
-          onChangeText={(text) => onChangeHandler(text)}
-        />
+    <View>
+      <View style={styles.container}>
+        <View style={styles.searchBar}>
+          {/* search Icon */}
+          <Feather
+            name="search"
+            size={20}
+            color="black"
+            style={{ marginLeft: 1 }}
+          />
+          {/* Input field */}
+          <TextInput
+            style={styles.input}
+            placeholder="Search"
+            value={text}
+            onChangeText={(text) => onChangeHandler(text)}
+          />
+          {/* {meds && meds.map((med, i) => <View key={i}>{med.value}</View>)} */}
+        </View>
       </View>
+      {meds != null
+        ? meds.map((med, i) => (
+            <Text key={i} style={styles.suggestions}>
+              {med.value}
+            </Text>
+          ))
+        : null}
     </View>
   );
 };
@@ -64,5 +87,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginLeft: 10,
     width: "90%",
+  },
+  suggestions: {
+    flexDirection: "column",
+    padding: 5,
   },
 });
