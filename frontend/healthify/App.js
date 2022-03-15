5e288264de26e312c305c3264a203323f6079faa
anirudhlakhotia <sanjeev196945@gmail.com>
2022-03-15 10:33:59 +0530

    :bug: Fixed merge headers

--- frontend/healthify/App.js
+++ frontend/healthify/App.js
@@ -1,26 +1,20 @@
-<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import SignUp from './containers/SignUp';
 // import ValidationComponent from 'react-native-form-validator'
-=======
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Image, View, Dimensions, Text } from "react-native";
 const { width, height } = Dimensions.get("window");
 import Verify from "./Verify";
->>>>>>> ab606debf190555ec657f577e1ad1172ec4d1410
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <View style={styles.SignUp}>
+    <View>
+    {/* <View style={styles.SignUp}> */}
       {/* <Text style={styles.text}>This is Healthify!</Text> */}
-    <SignUp/>
+    {/* <SignUp/> */}
       <StatusBar style="auto" />
-    </View>
-=======
+    {/* </View> */}
     <Verify email = "examplemail@gmail.com"/>
->>>>>>> ab606debf190555ec657f577e1ad1172ec4d1410
+    </View>
   );
 }
 
@@ -40,7 +34,6 @@ const styles = StyleSheet.create({
     width: 0.8 * width,
     height: 0.8 * width,
   },
-<<<<<<< HEAD
   text:{
     fontSize: 30,
     color: '#99dfb2',
@@ -55,7 +48,4 @@ const styles = StyleSheet.create({
   }
 });
 
-=======
-});
 export default App;
->>>>>>> ab606debf190555ec657f577e1ad1172ec4d1410
