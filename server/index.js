67452fd12cf26397c20d40c0ec0f6c207000cbf5
anirudhlakhotia <sanjeev196945@gmail.com>
2022-04-01 19:32:45 +0530

    :sparkles: Added Levenshtein Distance-Based approach for finding similar names

--- server/index.js
+++ server/index.js
@@ -232,13 +232,58 @@ async function getPharmeasy(medName) {
   for(i=0;i<names.length;i++){
    console.log(`${names[i]}: price:${prices[i]}, image:${images[i]}`)
   }
-  await browser.close()
- 
+  await browser.close().then(() => {
+    console.log("Browser closed");
+    return {names,prices,images}
+  }
+  );
 }
 
-// getPharmeasy("crocin");
-getNetMeds("crocin");
+// data = getPharmeasy("crocin");
+// console.log(data);
+// getNetMeds("crocin");
 
+
+//Chedk similarity of two strings based on levenshtein distance (Stackoverflow)
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+console.log(similarity("crocin pain relief tablet 15's", "crocin advance 500mg 20 tablets")); //Remove Strip Of, Bottle Of from the name if it has
 app.listen(3001, () => {
   console.log(`Server is running at ${port}`);
 });
